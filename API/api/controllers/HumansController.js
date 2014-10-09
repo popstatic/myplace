700c697e476ae6e3c5fc32a76e7d0451ae910976
root <root@myplace>
2014-10-09 19:27:28 +0200

    hello world

---
+++ API/api/controllers/HumansController.js
@@ -0,0 +1,11 @@
+/**
+ * HumansController
+ *
+ * @description :: Server-side logic for managing humans
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
